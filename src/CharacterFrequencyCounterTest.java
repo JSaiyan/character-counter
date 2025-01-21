f45731cf30bce248beb3bfeae9ed64ec49924d5c
JSaiyan <183049245+JSaiyan@users.noreply.github.com>
2025-01-20 18:19:43 -0800

    project completed

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -1,7 +1,30 @@
 import static org.junit.Assert.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
-public class CharacterFrequencyCounterTest {
+
+public class CharacterFrequencyCounterTest 
+{
+    @Test
+    public void testLAppearsThreeTimesInHelloWorld()
     
-}
+        {   //Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("Hello World");
+        //Act  
+        int actualCount = counter.getFrequency('l');
+        //Assert
+        assertEquals(3, actualCount);
+        }
+
+    @Test
+    public void testNonOccuringCharacterPercentageIsZero()
+        {
+        //Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hi there");
+        //Act
+        double actualPercentage = counter.getRelativePercentage('z');
+        //Assert
+        assertEquals(0.0, actualPercentage, 0.0001);
+        }
+
+}    
+    
